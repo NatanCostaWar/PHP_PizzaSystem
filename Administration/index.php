06912f3e12acf58c5ac5ccaa3563880c11d27e86
NatanCosta <natancosta10-2011@hotmail.com>
2019-09-18 20:00:47 -0300

    adding pizza menu and client list to ADM home page

--- Administration/index.php
+++ Administration/index.php
@@ -5,7 +5,71 @@
     <body class="background">
 
         <div id="main">
-            
+        	<div class="jumbotron jumbotron-fluid">
+				<div class="container">
+			    	<center>
+                		<h1 class="display-6">Pizza<span class="badge badge-danger">Menu</span></h1>
+                	</center>
+
+                	<hr class='my-4'>
+        		
+		        <?php
+				include("../connect.php");
+
+				$query = "SELECT * FROM db_pizzasystem.pizza";
+				$result = mysqli_query($connection ,$query) or die(mysql_error());
+
+				while($fetch = mysqli_fetch_row($result)){
+		        	echo "<div class='row'>
+		                    <div class='col-sm-12 col-md-12 col-lg-12'>
+		                        <h1 style='float:right'>" . "$" . $fetch[2] . "</h1>
+		                        <h1>" . $fetch[0] . " - " . $fetch[1] . "</h1>
+		                        <small>" . $fetch[3] . "</small>
+		                    </div>
+		                </div>";
+
+					echo "<hr class='my-4'>";
+
+			    }
+				?>
+        		</div>
+			</div>
+
+
+
+			<hr class="my-4">
+
+
+
+			<div class="jumbotron jumbotron-fluid">
+				<div class="container">
+			    	<center>
+                		<h1 class="display-6">Client<span class="badge badge-danger">List</span></h1>
+                	</center>
+
+                	<hr class='my-4'>
+        		
+		        <?php
+				include("../connect.php");
+
+				$query = "SELECT * FROM db_pizzasystem.client";
+				$result = mysqli_query($connection ,$query) or die(mysql_error());
+
+				while($fetch = mysqli_fetch_row($result)){
+		        	echo "<div class='row'>
+		                    <div class='col-sm-12 col-md-12 col-lg-12'>
+		                        <h1 style='float:right'>" . $fetch[3] . "</h1>
+		                        <h1>" . $fetch[1] . "</h1>
+		                        <small>" . $fetch[2] . "</small>
+		                    </div>
+		                </div>";
+
+					echo "<hr class='my-4'>";
+
+			    }
+				?>
+        		</div>
+			</div>
         </div>
 
         <div id="info">
