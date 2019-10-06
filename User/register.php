27506d482a8cfd089a88aaae8dd22e7ffaf7c7a8
NatanCosta <natancosta10-2011@hotmail.com>
2019-10-06 18:40:03 -0300

    removing database connect error

--- User/register.php
+++ User/register.php
@@ -10,7 +10,7 @@ $user = mysqli_real_escape_string($connection, $_POST["username"]);
 $password = mysqli_real_escape_string($connection, $_POST["password"]);
 
 #$query = "SELECT id, name FROM user WHERE name = '{$user}' and password = MD5('{$password}')";
-$query = "INSERT INTO exemple.user (id, name, password) VALUES (NULL, '{$user}', MD5('{$password}'))";
+$query = "INSERT INTO db_pizzasystem.user (id, name, password) VALUES (NULL, '{$user}', MD5('{$password}'))";
 $result = mysqli_query($connection, $query);
 if($result){
     header("Location: loginpage.php");
--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 $host = "127.0.0.1";
 $user = "root";
 $password = "usbw";
-$database = "Exemple";
+$database = "db_pizzasystem";
 $port = 3307;
 
 $connection = mysqli_connect($host, $user,$password, $database, $port) or die(mysqli_connect_errno())
